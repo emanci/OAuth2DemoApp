22a855ebf3261ee7aedb6544421218ee60c10487
puzhengchao <zhengchaopu@gmail.com>
2016-11-21 00:33:16 +0800

    Update

---
+++ app/Controllers/SettingController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: PhilPu <zhengchaopu@gmail.com>
+ * Date: 2016/11/16.
+ */
+
+namespace App\Controllers;
+
+class SettingController
+{
+    public function profile()
+    {
+        array_map(
+            function () {
+                return '';
+            }
+        );
+    }
+}
